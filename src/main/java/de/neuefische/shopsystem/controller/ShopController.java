e4cf1b9b9a784fc3b5be4946b352b8f3d7f66d88
Gerd Lödige <gerd.loedige@posteo.de>
2023-05-26 16:10:36 +0200

    ShopController mit Konstruktor befüllt.

--- src/main/java/de/neuefische/shopsystem/controller/ShopController.java
+++ src/main/java/de/neuefische/shopsystem/controller/ShopController.java
@@ -1,2 +1,16 @@
-package de.neuefische.shopsystem.service;public class ShopController {
+package de.neuefische.shopsystem.controller;
+
+import de.neuefische.shopsystem.service.ShopService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/shopservice")
+public class ShopController {
+    private final ShopService shopService;
+
+    public ShopController(ShopService shopService) {
+        this.shopService = shopService;
+    }
 }
