54fb306bab787f4b2db446ec892d5cd915956a97
Gerd Lödige <gerd.loedige@posteo.de>
2023-05-26 16:29:57 +0200

    Get-Mappings ergänzt.

--- src/main/java/de/neuefische/shopsystem/controller/ShopController.java
+++ src/main/java/de/neuefische/shopsystem/controller/ShopController.java
@@ -1,16 +1,34 @@
 package de.neuefische.shopsystem.controller;
 
+import de.neuefische.shopsystem.model.Order;
+import de.neuefische.shopsystem.model.Product;
 import de.neuefische.shopsystem.service.ShopService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/shopservice")
+@RequestMapping("/api/shop")
 public class ShopController {
     private final ShopService shopService;
 
     public ShopController(ShopService shopService) {
         this.shopService = shopService;
+    }
+    @GetMapping("/orders/{id}")
+    public Order getOrderById(@PathVariable String id){
+        return shopService.getOrderById(id);
+    }
+    @GetMapping("/products/{id}")
+    public Product getProductById(@PathVariable String id){
+        return shopService.getProductById(id);
+    }
+    @GetMapping("/products")
+    public List<Product> listProducts(){
+        return shopService.listProducts();
+    }
+    @GetMapping("/orders")
+    public List<Order> listOrders(){
+        return shopService.listOrders();
     }
 }
